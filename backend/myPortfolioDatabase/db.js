63e99d49f23ddb8f8f46da79033df46063ddc275
MikiasEyayaw <mikiaseyayaw123@gmail.com>
2025-04-16 14:49:46 +0300

    Update db.js

--- backend/myPortfolioDatabase/db.js
+++ backend/myPortfolioDatabase/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-      await mongoose.connect('mongodb://localhost:27017/myPortfolio');
+      await mongoose.connect(process.env.MONGO_URI);
       console.log("MongoDB connected");
     } catch (err) {
       console.error(err);
